5277569a6d7a7a2371891fb388ebf7817c4fddde
Alex123 <you@example.comsamokhin_ay@techmoney.ru>
2022-11-30 20:41:46 +0300

    dz1 completed

--- Example dz1.1/Program.cs
+++ Example dz1.1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+//Console.WriteLine("Hello, World!");
+
+int NumberA = int.Parse(Console.ReadLine());
+int NumberB = int.Parse(Console.ReadLine());
+
+if (NumberB == NumberA)
+{ Console.WriteLine("Введенные числа равны");}
+
+else 
+ if (NumberA > NumberB)
+{ Console.WriteLine($"Большее число - {NumberA}");
+Console.WriteLine($"Меньшее число - {NumberB}");}
+else
+{
+Console.WriteLine($"Большее число - {NumberB}"); 
+Console.WriteLine($"Меньшее число - {NumberA}");
+}
