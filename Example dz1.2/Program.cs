d180dc6ea99e5fc648f3637c92aecf26b3cd490f
Alex123 <you@example.comsamokhin_ay@techmoney.ru>
2022-11-30 21:43:59 +0300

    Example dz1.2 complete

---
+++ Example dz1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int NumberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int NumberC = int.Parse(Console.ReadLine());
+
+if (NumberB == NumberA && NumberA == NumberC)
+{   Console.WriteLine("Введенные числа равны"); }
+
+if (NumberB < NumberA && NumberA > NumberC)
+{   Console.WriteLine($"максимальное число - {NumberA}"); }
+else
+if (NumberA < NumberB && NumberB > NumberC)
+{   Console.WriteLine($"максимальное число - {NumberB}"); }
+else
+if (NumberA < NumberC && NumberC > NumberA)
+{
+    Console.WriteLine($"Максимальное число - {NumberC}");
+}
